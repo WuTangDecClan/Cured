cf66f82eb31ce01a86e3373b46ba82871dc46512
Zhanna Kresteva <kresteva.astana@gmail.com>
2020-04-17 17:48:43 +0300

    Refactoring *Login Activity, Main Activity

---
+++ app/src/main/java/com/example/cured/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.cured;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
--- app/src/main/java/com/example/cured/MainActivity.java
+++ app/src/main/java/com/example/cured/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.cured;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.widget.CalendarView;
 
-import android.os.Bundle;
+        import androidx.annotation.NonNull;
+        import androidx.annotation.Nullable;
+        import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "CalendarActivity";
+
+    private CalendarView mCalendarView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + month + "/" + year;
+                Log.d(TAG, "date is :" + date);
+            }
+        });
+
     }
 }
