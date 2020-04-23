0cab9de58c08f014b86159ec6468964f50ab4839
WuTangDecClan <declan.thorne98@gmail.com>
2020-04-23 10:06:18 +0100

    Began work on Splash Screen. Moved Main Activity & Main Activity XML file to Design & activity_design XML file.

---
+++ app/src/main/java/com/example/cured/Design.java
@@ -0,0 +1,37 @@
+package com.example.cured;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Design extends AppCompatActivity {
+
+    private static final String TAG = "CalendarActivity";
+
+    private CalendarView mCalendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_design);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + month + "/" + year;
+                Log.d(TAG, "date is :" + date);
+            }
+        });
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){ // If there are no current user
+            // Go to Login view or Sign up view or some other view..
+            //FirebaseAuth.getInstance().signOut(); // This is logout method
+        }
+    }
+}
