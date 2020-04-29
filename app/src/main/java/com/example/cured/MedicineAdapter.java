bba5b5d188369c317e44d0c29fc5691906d3b947
An98 <022543@naver.com>
2020-04-29 21:39:09 +0900

    switch enabled

--- app/src/main/java/com/example/cured/MedicineAdapter.java
+++ app/src/main/java/com/example/cured/MedicineAdapter.java
@@ -1,19 +1,25 @@
 package com.example.cured;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class MedicineAdapter extends RecyclerView.Adapter<MedicineAdapter.MyViewHolder>{
     Context context;
     ArrayList<MyMedicine> myMedicine;
+    int h,m;
+    String t;
 
     public MedicineAdapter(Context c, ArrayList<MyMedicine> p) {
         context = c;
@@ -33,6 +39,22 @@ public class MedicineAdapter extends RecyclerView.Adapter<MedicineAdapter.MyView
         //myViewHolder.medicine_dose.setText(myMedicine.get(i).getMedicine_dose());
         myViewHolder.medicine_time.setText(myMedicine.get(i).getMedicine_time());
 
+        myViewHolder.switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    StringTokenizer st = new StringTokenizer((String) myViewHolder.medicine_time.getText(),"::");
+                    h = Integer.parseInt(st.nextToken());
+                    m = Integer.parseInt(st.nextToken());
+                    t=Integer.toString(h)+" : "+Integer.toString(m);
+
+                    Log.e("time",t);
+                }
+                else{
+
+                }
+            }
+        });
 //        final String getMedicine_title = myMedicine.get(i).getMedicine_title();
 //        final String getMedicine_dose = myMedicine.get(i).getMedicine_dose();
 //        final String getMedicine_time = myMedicine.get(i).getMedicine_time();
@@ -60,12 +82,14 @@ public class MedicineAdapter extends RecyclerView.Adapter<MedicineAdapter.MyView
     class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView medicine_title, medicine_dose, medicine_time;
+        Switch switch1;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             medicine_title = (TextView) itemView.findViewById(R.id.medicine_title);
             medicine_dose = (TextView) itemView.findViewById(R.id.medicine_dose);
             medicine_time = (TextView) itemView.findViewById(R.id.medicine_time);
+            switch1 = (Switch) itemView.findViewById(R.id.switch1);
         }
     }
 }
