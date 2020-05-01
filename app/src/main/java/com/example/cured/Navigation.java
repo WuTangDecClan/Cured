90ce87c7d88d3cfac724b63312e769bb84646055
An98 <022543@naver.com>
2020-05-02 02:35:09 +0900

    Navigation on DiaryActivity

--- app/src/main/java/com/example/cured/Navigation.java
+++ app/src/main/java/com/example/cured/Navigation.java
@@ -1,7 +1,12 @@
 package com.example.cured;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,48 +14,77 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-public class Navigation extends AppCompatActivity {
-    ListView listview = null;
+public class Navigation  {
+    Context context;
+    ListView listview;
+    DrawerLayout drawer;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public Navigation(Context c, ListView l,DrawerLayout d){
+        context = c;
+        listview = l;
+        drawer = d;
+    }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity);
 
-        final String[] items = {"add", "delete", "update", "personal_info"};
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
+   public void setN(){
 
-        listview = (ListView) findViewById(R.id.drawerList);
+        final String[] items = {"Add", "Edit", "Diary", "Main", "LogOut"};
+        ArrayAdapter adapter = new ArrayAdapter(context, R.layout.list, items);
+
         listview.setAdapter(adapter);
 
         listview.setOnItemClickListener(new ListView.OnItemClickListener() {
 
-            @Override
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
+           @Override
+           public void onItemClick(AdapterView parent, View v, int position, long id) {
 
-                //TextView contentTextview = (TextView) findViewById(R.id.drawer_content);
+               switch (position) {
+                   case 0: // Add
+                       Intent a = new Intent(context, NewMedicineActivity.class);
+                       context.startActivity(a);
+                       break;
+                   case 1: // Edit
+                       Intent e = new Intent(context, MyMedicine.class);
+                       context.startActivity(e);
+                       break;
+                   case 2: // Diary
+                       Intent b = new Intent(context, DiaryActivity.class);
+                       context.startActivity(b);
+                       break;
+                   case 3: // Main
+                       Intent d = new Intent(context, MainActivity.class);
+                       context.startActivity(d);
+                       break;
+                   case 4: //Logout
+                       new AlertDialog.Builder(context)
+                               .setTitle("logout").setMessage("Do you want to logout?")
+                               .setPositiveButton("LogOut", new DialogInterface.OnClickListener() {
+                                   public void onClick(DialogInterface dialog, int whichButton) {
+                                       Intent i = new Intent(context, LoginActivity.class);
+                                       i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                                       context.startActivity(i);
+                                   }
+                               })
+                               .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                   public void onClick(DialogInterface dialog, int whichButton) {
 
-                switch (position) {
-                    case 0: // add
-                        Toast.makeText(getApplicationContext(), "add", Toast.LENGTH_LONG).show();
-                        break;
-                    case 1: // delete
-                        Toast.makeText(getApplicationContext(), "delete", Toast.LENGTH_LONG).show();
-                        break;
-                    case 2: // update
-                        Toast.makeText(getApplicationContext(), "update", Toast.LENGTH_LONG).show();
-                        break;
-                    case 3: // personal_info
-                        Toast.makeText(getApplicationContext(), "personal_info", Toast.LENGTH_LONG).show();
-                        break;
-                }
-                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
-                drawer.closeDrawer(Gravity.LEFT);
-            }
+                                   }
+                               })
+                               .show();
+                       break;
+                   case 5:
+                       Intent m = new Intent(context, MyMedicine.class);
+                       context.startActivity(m);
+                       break;
+               }
+               drawer.closeDrawer(Gravity.LEFT);
+           }
         });
-    }
+   }
+
+
 }
